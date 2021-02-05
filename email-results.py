c917cc99711c825e55136d22b3f170eb56d51bcd
Alexander Zhang <zhangalexy@gmail.com>
2021-02-05 10:20:52 -0800

    Add greeting, more efficient string building

--- email-results.py
+++ email-results.py
@@ -25,17 +25,17 @@ with smtplib.SMTP('smtp.gmail.com', 587) as server:
             msg['Subject'] = f'CVJMT Results For {row["first_name"]} {row["last_name"]}'
             msg['From'] = from_email
             msg['To'] = row['email']
-            body = (
+            msg.set_content(
+                f'Hello {row["first_name"]},\n\n'
+                f'Here are the results of your CVJMT submission.\n'
                 f'First name: {row["first_name"]}\n'
                 f'Last name: {row["last_name"]}\n'
                 f'School: {row["school"]}\n'
                 f'Grade: {row["grade"]}\n'
                 f'Division: {"Middle School" if row["division"] == "" else row["division"]}\n\n'
-                f'Points for each problem:\n'
-            )
-            for i in range(1, 26):
-                body += f'Problem {i}: {row[str(i)]}\n'
-            body += (
+                f'Points for each problem:\n' +
+                ''.join([f'Problem {i}: {row[str(i)]}\n' for i in range(1, 26)]) +
+                f'\n'
                 f'Score: {row["score"]}\n\n'
                 f'The answer keys and solutions are available on our website at https://cvhsmath.weebly.com/cvjmt.html. '
                 f'If we made any mistakes, please email us by Friday, February 12th. '
@@ -43,5 +43,4 @@ with smtplib.SMTP('smtp.gmail.com', 587) as server:
                 f'Thank you for participating!\n\n'
                 f'(This is an automated message.)'
             )
-            msg.set_content(body)
             server.send_message(msg)
